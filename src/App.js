f33daac07aadbfde4ff371fa9836cd699ac09d7f
Joshua <joshuamayowa23@yahoo.com>
2022-09-13 09:55:41 +0100

    useEffect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import Card from './card'
-import robots from './robots'
-import { useState } from 'react';
 
 function App() {
-  const [robot, setRobot] = useState(robots);
   return (
     <div className="App">
-      <Card robots={robot}/>
+      <Card />
     </div>
   );
 }
--- src/card.jsx
+++ src/card.jsx
@@ -1,12 +1,23 @@
-import React, {useState } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, {useEffect, useState } from 'react';
+import Scroll from './scroll';
 
 
-const Card = ({robots}) => {
+const Card = () => {
+    const [robots, setRobots] = useState([]);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [newRobots, setNewRobots] = useState([]);
     const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(robot => {
+                setRobots(robot)
+            });
+        }, 1000)
+    }, []);
 
     const showingRobot = query === ''
         ? robots
@@ -26,96 +37,97 @@ const Card = ({robots}) => {
         }
     }
 
-    
-    return (
-        <div className='todo-container'>
-            <header>
-                <h1>My RoboFriends</h1>
-                <div className="search">
-                    <input type="text"
-                            placeholder='Search friends'
-                            onChange={(e) => setQuery(e.target.value)}
-                        />
-                    <button>Search</button>
-                </div>
-            </header>
+    if (robots.length === 0) {
+        return <h1>loading ...</h1>
+    } else {
+        return (
+            <div className='todo-container'>
+                <header>
+                    <h1>My RoboFriends</h1>
+                    <div className="search">
+                        <input type="text"
+                                placeholder='Search friends'
+                                onChange={(e) => setQuery(e.target.value)}
+                            />
+                        <button>Search</button>
+                    </div>
+                </header>
 
-            <form className='form'>
-                <div className='form-control'>
-                    <label
-                        htmlFor="name">
-                            Name : &nbsp;
-                    </label>
-                    <input 
-                        type="text"
-                        id='name'
-                        value={name}
-                        onChange={(e)=>setName(e.target.value)}
-                        />
-                </div>
-                <div className='form-control'>
-                    <label
-                        htmlFor="email">
-                            Email : &nbsp;
-                    </label>
-                    <input
-                        type="text"
-                        id='email'
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}
-                        />
-                </div>
-                <button
-                    className='btn'
-                    type='submit'
-                    onClick={handleSubmit}>
-                        Add
-                </button>
-            </form>
+                <form className='form'>
+                    <div className='form-control'>
+                        <label htmlFor="name">Name : &nbsp;</label>
+                        <input 
+                            type="text"
+                            id='name'
+                            value={name}
+                            onChange={(e)=>setName(e.target.value)}
+                            />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="email">Email : &nbsp;</label>
+                        <input
+                            type="text"
+                            id='email'
+                            value={email}
+                            onChange={(e)=>setEmail(e.target.value)}
+                            />
+                    </div>
+                    <button
+                        className='btn'
+                        type='submit'
+                        onClick={handleSubmit}>
+                            Add
+                    </button>
+                </form>          
 
-            <div className='card'>
-                {showingRobot.map(
-                    (robot) => {
-                        const {id, name, email} = robot;
-                        return (
-                            <div
-                                className='card-container'
-                                key={id}>
-                                <img
-                                    src={`https://robohash.org/${name}`}
-                                    alt="robot" />
-                                <h3>
-                                    {name}
-                                </h3>
-                                <p>
-                                    {email}
-                                </p>
-                            </div>
-                        )
-                })}
+                <Scroll>
+                    <div className='card'>
+                        {showingRobot.map(
+                            (robot) => {
+                                const {id, name, email} = robot;
+                                return (
+                                    <div
+                                        className='card-container'
+                                        key={id}>
+                                        <img
+                                            src={`https://robohash.org/${name}`}
+                                            alt="robot" />
+                                        <h3>
+                                            {name}
+                                        </h3>
+                                        <p>
+                                            {email}
+                                        </p>
+                                    </div>
+                                )
+                        })}
+                        
+                        {newRobots.map(
+                            (newRobot) => {
+                                const {id, name, email} = newRobot;
+                                return (
+                                    <div
+                                        className='card-container'
+                                        key={id}>
+                                        <img
+                                            src={`https://robohash.org/${name}`}
+                                            alt="robot" />
+                                        <h3>
+                                            {name}
+                                        </h3>
+                                        <p>
+                                            {email}
+                                        </p>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                </Scroll>
+
                 
-                {newRobots.map(
-                    (newRobot) => {
-                        const {id, name, email} = newRobot;
-                        return (
-                            <div
-                                className='card-container'
-                                key={id}>
-                                <img
-                                    src={`https://robohash.org/${name}`}
-                                    alt="robot" />
-                                <h3>
-                                    {name}
-                                </h3>
-                                <p>
-                                    {email}
-                                </p>
-                            </div>
-                        )
-                })}
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 
